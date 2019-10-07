ff47c47e69a3c906f6d7f45aac51443f2e5ce6c0
Yi SoJeong <soj980312@gmail.com>
2019-10-08 01:17:02 +0900

    y오름차순, x오름차순 정렬하기

---
+++ PS/11651_좌표 정렬하기 2.py
@@ -0,0 +1,12 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+sys.stdin = open('input.txt', 'r')
+
+n = int(input())
+a = [list(map(int, input().split())) for _ in range(n)]
+
+a.sort(key=lambda x: (x[1], x[0]))
+
+for pair in a:
+    print(' '.join(map(str,pair)))
