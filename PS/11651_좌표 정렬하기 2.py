a5bdb5892358d1adff1773d2cc1ec2b9719df349
Yi SoJeong <soj980312@gmail.com>
2019-10-08 01:20:28 +0900

    y좌표 오름차순, x좌표 오름차순 정렬하기

--- PS/11651_좌표 정렬하기 2.py
+++ PS/11651_좌표 정렬하기 2.py
@@ -1,7 +1,7 @@
 import sys
 input = lambda: sys.stdin.readline().rstrip()
 
-sys.stdin = open('input.txt', 'r')
+# sys.stdin = open('input.txt', 'r')
 
 n = int(input())
 a = [list(map(int, input().split())) for _ in range(n)]
