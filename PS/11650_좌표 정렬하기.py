dc8c7a1212aefb73ef5dfeca5c111ffae14104f8
Yi SoJeong <soj980312@gmail.com>
2019-10-07 23:59:27 +0900

    x좌, y좌표 오름차순 정렬하기

--- PS/11650_좌표 정렬하기.py
+++ PS/11650_좌표 정렬하기.py
@@ -2,7 +2,7 @@ import sys
 input = lambda: sys.stdin.readline().rstrip()
 
 n = int(input())
-a = [list(map(int,input().split())) for _ in range(n)]
+a = [list(map(int, input().split())) for _ in range(n)]
 
 a.sort(key=lambda x: (x[0], x[1]))
 
