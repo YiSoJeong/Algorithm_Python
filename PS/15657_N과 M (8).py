3075a880e2e8bcf669fad3e649802a4cdc8b35c4
Yi SoJeong <soj980312@gmail.com>
2019-10-13 18:43:42 +0900

    N개의 입력받은 수 중 중복허용하여 길이가 M인 비내림차순 수열만들기(중복조합)

---
+++ PS/15657_N과 M (8).py
@@ -0,0 +1,11 @@
+import sys
+from itertools import combinations_with_replacement as com
+input = lambda: sys.stdin.readline().rstrip()
+
+sys.stdin = open('input.txt', 'r')
+
+n, m = map(int, input().split())
+arr = sorted(list(map(int, input().split())))
+
+for case in com(arr, m):
+    print(' '.join(map(str, case)))
