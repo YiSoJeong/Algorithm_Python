6c94e62382ace25d588da4ac95841d5a263ce7da
Yi SoJeong <soj980312@gmail.com>
2019-10-13 17:08:30 +0900

    섞어먹으면 안되는 조합 피해서 n개중 3개 아이스크림 고르기

--- PS/2422_한윤정이 이탈리아에 가서 아이스크림을 사먹는데.py
+++ PS/2422_한윤정이 이탈리아에 가서 아이스크림을 사먹는데.py
@@ -6,13 +6,13 @@ input = lambda: sys.stdin.readline().rstrip()
 sys.stdin = open('input.txt', 'r')
 
 n, m = map(int, input().split())
-no = [[0]*(n+1) for _ in range(n+1)]
+no = [[False]*(n+1) for _ in range(n+1)]
 cnt = 0
 
 for i in range(m):
     x, y = map(int, input().split())
-    no[x][y] = 1
-    no[y][x] = 1
+    no[x][y] = True
+    no[y][x] = True
 
 for case in com(range(1, n+1), 3):
     if no[case[0]][case[1]] or no[case[0]][case[2]] or no[case[1]][case[2]]:
