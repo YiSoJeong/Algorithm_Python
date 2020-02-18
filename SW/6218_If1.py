f5319d9378347546c9cb5af7a5c1e57446f5fb71
Yi SoJeong <soj980312@gmail.com>
2020-02-18 22:34:34 +0100

    소수 구할때 반복문 도는 수 감소시키기

--- SW/6218_If1.py
+++ SW/6218_If1.py
@@ -5,8 +5,8 @@ for i in range(1, a+1):
     if a % i == 0:
         s.add(i)
         s.add(a//i)
-        # if s[len(s)-1] >= i:
-        #     break
+        if a//i <= i:
+            break
 
 for x in sorted(s):
     print("%d(은)는 %d의 약수입니다." % (x, a))
