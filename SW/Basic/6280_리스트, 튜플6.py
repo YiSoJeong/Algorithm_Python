b822f2e0c639c16cce0482409c30af70c680aff8
Yi SoJeong <soj980312@gmail.com>
2020-02-25 14:28:33 +0100

    리스트에 약수담기

--- SW/Basic/6280_리스트, 튜플6.py
+++ SW/Basic/6280_리스트, 튜플6.py
@@ -1,8 +1,7 @@
 n = int(input())
 div = []
 
-for i in range(1, round(n ** 0.5) + 1): # int(n ** 0.5 + 1.5)와 동
+for i in range(1, round(n ** 0.5) + 1): # int(n ** 0.5 + 1.5)와 동일
     div.extend([i, n//i])
 
 print(sorted(div))
-일
