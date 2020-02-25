7ea50839b9707e4407be024e3ec39829394aed5d
Yi SoJeong <soj980312@gmail.com>
2020-02-26 00:30:05 +0100

    리스트, 튜플 생성에서의 궁금증

--- SW/Basic/6292_리스트, 튜플16.py
+++ SW/Basic/6292_리스트, 튜플16.py
@@ -2,3 +2,9 @@ a = input()
 
 print(list(map(int, a.split(', '))))
 print(tuple(map(int, a.split(', '))))
+
+# Why error? 뒤에 나오는 자료구조에 아무 항목이 들어가지 않음 ㅠ.
+b = map(int, input().split(', '))
+
+print(list(b))
+print(tuple(b))
