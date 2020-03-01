633cab962920010407053f749d89c312cc8c24cf
Yi SoJeong <soj980312@gmail.com>
2020-03-01 10:55:12 +0100

    클래스를 이용하여 국적 출력하기

---
+++ SW/Basic/6208_객체지향2.py
@@ -0,0 +1,12 @@
+class Nationality:
+    def __init__(self):
+        self.__nationality = '대한민국'
+
+    def printNationality(self):
+        print(self.__nationality)
+
+
+person = Nationality()
+
+person.printNationality()
+person.printNationality()
