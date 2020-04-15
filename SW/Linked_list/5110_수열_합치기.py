d65db5baa6cfcb709342f762a00004c306bb91f1
Yi SoJeong <soj980312@gmail.com>
2020-04-16 00:27:04 +0900

    리스트 슬라이싱 주석추가

--- SW/Linked_list/5110_수열_합치기.py
+++ SW/Linked_list/5110_수열_합치기.py
@@ -16,8 +16,9 @@ for t in range(1, T+1):
         tmp = list(map(int, input().split()))
         for i in range(len(li)):
             if tmp[0] < li[i]:
-                li[i:i] = tmp #
+                li[i:i] = tmp  # 리스트에 리스트 추가하기(원소만)
                 break
             elif i == len(li)-1 and tmp[0] > li[i]:
                 li.extend(tmp)
-    print('#{} {}'.format(t, ' '.join(map(str, li[-1:-11:-1]))))
+    print('#{} {}'.format(t, ' '.join(map(str, li[-1:-11:-1]))))  # 리스트 역순으로 출력하
+기
