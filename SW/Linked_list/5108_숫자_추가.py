5966ed3fdf0f00d6d34a324d7934b75b2677367b
Yi SoJeong <soj980312@gmail.com>
2020-04-15 23:39:45 +0900

    주석 비활성화

--- SW/Linked_list/5108_숫자_추가.py
+++ SW/Linked_list/5108_숫자_추가.py
@@ -7,16 +7,16 @@ sys.stdin = open('sample_input.txt', 'r')
 # 조합 :
 
 # sol1 : list의 insert함수를 이용해서 쉽게 추가 but 느림
-# T = int(input())
-# for t in range(1, T+1):
-#     n, m, l = map(int, input().split())
-#     li = list(input().split())
-#
-#     for _ in range(m):
-#         idx, val = map(int, input().split())
-#         li.insert(idx, val)
-#
-#     print('#{} {}'.format(t, li[l]))
+T = int(input())
+for t in range(1, T+1):
+    n, m, l = map(int, input().split())
+    li = list(input().split())
+
+    for _ in range(m):
+        idx, val = map(int, input().split())
+        li.insert(idx, val)
+
+    print('#{} {}'.format(t, li[l]))
 
 # sol2 : linked list로 원소 추가
 # https://www.fun-coding.org/DS&AL1-4.html : 파이썬은 리스트 타입이 링크드 리스트의 기능을 모두 지원한다고 함;
