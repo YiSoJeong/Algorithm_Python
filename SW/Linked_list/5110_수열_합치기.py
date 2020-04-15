48bd5a64a0f41993ab9a017d61b132a5196f0d57
Yi SoJeong <soj980312@gmail.com>
2020-04-16 00:23:15 +0900

    수열의 첫 번째 원소의 크기에 따라 기존의 수열에 추가하기

---
+++ SW/Linked_list/5110_수열_합치기.py
@@ -0,0 +1,23 @@
+import sys
+sys.stdin = open('sample_input.txt', 'r')
+
+# 분류 : 리스트
+# 발상 : 수열 -> 리스트
+# 변형 : 수열의 첫 번째 원소의 크기에 따라 기존의 수열에 추가
+# 조합 : 정렬, 리스트 조작, 리스트 슬라이싱
+# 리스트 조작 : https://dojang.io/mod/page/view.php?id=2281
+# 리스트 슬라이싱 : https://dojang.io/mod/page/view.php?id=2208
+
+T = int(input())
+for t in range(1, T+1):
+    n, m = map(int, input().split())
+    li = list(map(int, input().split()))
+    for _ in range(n-1):
+        tmp = list(map(int, input().split()))
+        for i in range(len(li)):
+            if tmp[0] < li[i]:
+                li[i:i] = tmp #
+                break
+            elif i == len(li)-1 and tmp[0] > li[i]:
+                li.extend(tmp)
+    print('#{} {}'.format(t, ' '.join(map(str, li[-1:-11:-1]))))
