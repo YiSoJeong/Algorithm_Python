ab6c5486b77145138d0f7ef10527b2d00aa301b8
Yi SoJeong <soj980312@gmail.com>
2020-04-15 23:11:06 +0900

    리스트의 특정 인덱스에 원소 추가하기 (순차 리스트)

---
+++ SW/Linked_list/5108_숫자_추가.py
@@ -0,0 +1,19 @@
+import sys
+sys.stdin = open('sample_input.txt', 'r')
+
+# 분류 : Linked List
+# 발상 : 배열에 삽입이 많음 -> 링크드 리스트
+# 변형 : 수열의 인덱스에 숫자 추가
+# 조합 :
+
+# sol1 : list의 insert함수를 이용해서 쉽게 추가 but 느림
+T = int(input())
+for t in range(1, T+1):
+    n, m, l = map(int, input().split())
+    li = list(input().split())
+
+    for _ in range(m):
+        idx, val = map(int, input().split())
+        li.insert(idx, val)
+
+    print('#{} {}'.format(t, li[l]))
