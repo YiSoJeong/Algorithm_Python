7804ac39fc1333f327e54c414a419adda1581589
Yi SoJeong <soj980312@gmail.com>
2020-04-28 17:47:37 +0900

    이진탐색 주석추가

--- SW/Divide&Conquer/5207_이진 탐색.py
+++ SW/Divide&Conquer/5207_이진 탐색.py
@@ -3,13 +3,13 @@ sys.stdin = open('sample_input.txt', 'r')
 
 # 분류 : Binary Search
 # 발상 : 이진 탐색 -> 탐색 범위 반으로 좁히기
-# 변형 : 존재하는 원소에 대해 탐색 과정에서 양쪽 구간을 번갈아 선택하게 되는 숫자의 개
+# 변형 : 존재하는 원소에 대해 탐색 과정에서 양쪽 구간을 번갈아 선택하게 되는 숫자의 개수
 # 조합 : Divide & Conquer
 
 T = int(input())
 for t in range(1, T+1):
     N, M = map(int, input().split())
-    A = sorted(list(map(int, input().split())))
+    A = sorted(list(map(int, input().split())))  # 정렬된 상태로 저장
     B = list(map(int, input().split()))
     cnt = 0
 
